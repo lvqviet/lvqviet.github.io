c31e950ae1305888b0e555182dae2a939e1c8a53
lvqviet <61400407+lvqviet@users.noreply.github.com>
2020-02-24 14:33:11 +0700

    xong

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-$("#volume").slider({
-    min: 0,
-    max: 100,
-    value: 0,
-      range: "min",
-    slide: function(event, ui) {
-      setVolume(ui.value / 100);
-    }
-  });
-  
-  var myMedia = document.createElement('audio');
-  $('#player').append(myMedia);
-  myMedia.id = "myMedia";
-  
-  function playAudio(fileName, myVolume) {
-          myMedia.src = fileName;
-          myMedia.setAttribute('loop', 'loop');
-      setVolume(myVolume);
-      myMedia.play();
-  }
-  
-  function setVolume(myVolume) {
-  var myMedia = document.getElementById('myMedia');
-  myMedia.volume = myVolume;
-  };
-
-
 $(function()
 {
     var playerTrack = $("#player-track");
